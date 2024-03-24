3adaff25ec26b04e60b65b2a83f0a0b1d2a39df2
Loïc GUEZO <guezo.loic@gmail.com>
2024-03-24 20:58:30 +0100

    add res

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.random.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
@@ -10,6 +14,7 @@ import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
+
 
 public class Main {
     public static void main(String[] args) {
